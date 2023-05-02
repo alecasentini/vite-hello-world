3f412fd129146979d77ab238dd05cf3b333fcdf9
alecasentini <ale.casentini@gmail.com>
2023-05-02 17:09:51 +0200

    Aggiunta AppHero con img

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import AppTitle from './components/AppTitle.vue'
+import AppHero from './components/AppHero.vue'
 </script>
 
 <template>
-  <AppTitle />
+  <AppTitle></AppTitle>
+  <AppHero></AppHero>
 </template>
 
 <style scoped></style>
---
+++ src/components/AppHero.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    name: "AppHero",
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <img src="../assets/04.jpg" alt="Dark Charizard">
+</template>
+
+<style scoped>
+img {
+    height: 500px;
+    width: 380px;
+}
+</style>
